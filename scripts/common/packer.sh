f741c64d3d7e283b8e7be5203b40eb553e942120
Marcelo Correia Pinheiro <salizzar@gmail.com>
2014-11-08 19:15:12 -0200

    Updated packer script to be more smart getting latest version from site.

--- scripts/common/packer.sh
+++ scripts/common/packer.sh
@@ -1,13 +1,12 @@
 #! /usr/bin/env bash
 
-PACKER_PKG="0.5.2_linux_amd64.zip"
-PACKER_URL="https://dl.bintray.com/mitchellh/packer/$PACKER_PKG"
+PACKER_VER=`curl -s https://www.packer.io/downloads.html | grep 'Latest version' | egrep -o '[0-9]{1,2}\.[0-9]{1,2}\.[0-9]{1,2}'`
+PACKER_PKG="${PACKER_VER}_linux_amd64.zip"
+PACKER_URL="https://dl.bintray.com/mitchellh/packer/packer_$PACKER_PKG"
 PACKER_TMP="/tmp/$PACKER_PKG"
 PACKER_DIR="/usr/local/bin"
 
 curl --location --output $PACKER_TMP $PACKER_URL
 
-unzip -o $PACKER_TMP -d $PACKER_DIR
-
-rm $PACKER_TMP
+unzip -v -o $PACKER_TMP -d $PACKER_DIR
 
